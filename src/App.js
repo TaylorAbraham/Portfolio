c4f09ef6d66810cc8e9c4ecdd9e1387006c3af38
Taylor Abraham <taylor.abra8@gmail.com>
2021-02-06 01:33:38 -0500

    Foundation for projects section & small fixes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.scss';
 import 'cirrus-ui/dist/cirrus.min.css';
 import 'cirrus-ui/dist/cirrus-core.min.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHeart, faEnvelope, faFile } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope, faFile } from '@fortawesome/free-solid-svg-icons'
 import { faGithub, faLinkedin, faDev } from '@fortawesome/free-brands-svg-icons'
 
 function App() {
@@ -18,6 +18,7 @@ function App() {
           <div className="nav-right">
             <div className="nav-item text-center"><a href="!#about" className="u u-C">About</a></div>
             <div className="nav-item text-center"><a href="!#links" className="u u-C">Links</a></div>
+            <div className="nav-item text-center"><a href="!#projects" className="u u-C">Projects</a></div>
           </div>
         </div>
       </div>
@@ -32,7 +33,8 @@ function App() {
         </div>
       </section>
 
-      <section id="about" className="section">
+      <section id="about-sec" className="section">
+        <a id="about" class="anchor" />
         <div className="content">
           <div className="row level">
             <div className="col-4">
@@ -44,48 +46,85 @@ function App() {
               <h3>About</h3>
               <div className="divider" />
               <p>Hey there, I'm Taylor! A full-stack developer and former hackathon organizer. I'm always looking for opportunities to learn new technology or to teach others.</p>
-              <p>I have a broad variety of experience. Through internships, hackathons, and personal projects, I've learned how to build maintainable code and how to get things out of the door when needed. Check out <a href="https://github.com/TaylorAbraham" target="_blank" classname="u u-LR">my GitHub</a> for a small sample!</p>
+              <p>I have a broad variety of experience. Through internships, hackathons, and personal projects, I've learned how to build maintainable code and how to get things out of the door when needed. Check out <a href="https://github.com/TaylorAbraham" target="_blank" className="u u-LR">my GitHub</a> for a small sample!</p>
               <p>Other interests include meetups, game development, and dogs.</p>
             </div>
           </div>
         </div>
       </section>
 
-      <section id="links" className="section">
+      <section id="links-sec" className="section">
+        <a id="links" class="anchor" />
         <div className="content">
           <h3 className="u-center">My Links</h3>
           <div className="divider" />
           <div className="row level">
             <div className="col col-2">
-              <a href="https://github.com/TaylorAbraham" target="_blank" classname="utb utb-OLR"><FontAwesomeIcon icon={faGithub} size="5x" /><p className="link-text">Github</p></a>
+              <a href="https://github.com/TaylorAbraham" target="_blank"><FontAwesomeIcon icon={faGithub} size="5x" /><p className="link-text">Github</p></a>
             </div>
             <div className="col col-2">
-              <a href="https://www.linkedin.com/in/taylor-abra/" target="_blank" classname="utb utb-OLR"><FontAwesomeIcon icon={faLinkedin} size="5x" /><p className="link-text">LinkedIn</p></a>
+              <a href="https://www.linkedin.com/in/taylor-abra/" target="_blank"><FontAwesomeIcon icon={faLinkedin} size="5x" /><p className="link-text">LinkedIn</p></a>
             </div>
             <div className="col col-2">
-              <a href="https://devpost.com/TaylorAbraham/" target="_blank" classname="utb utb-OLR"><FontAwesomeIcon icon={faDev} size="5x" /><p className="link-text">DevPost</p></a>
+              <a href="https://devpost.com/TaylorAbraham/" target="_blank"><FontAwesomeIcon icon={faDev} size="5x" /><p className="link-text">DevPost</p></a>
             </div>
           </div>
           <div className="row level">
             <div className="col col-2">
-              <a href="mailto:taylor.abra8@gmail.com" target="_blank" classname="utb utb-OLR"><FontAwesomeIcon icon={faEnvelope} size="5x" /><p className="link-text">Email</p></a>
+              <a href="mailto:taylor.abra8@gmail.com" target="_blank"><FontAwesomeIcon icon={faEnvelope} size="5x" /><p className="link-text">Email</p></a>
             </div>
             <div className="col col-2">
-              <a href="TaylorAbraham.pdf" target="_blank" classname="utb utb-OLR"><FontAwesomeIcon icon={faFile} size="5x" /><p className="link-text">My Resume</p></a>
+              <a href="TaylorAbraham.pdf" target="_blank"><FontAwesomeIcon icon={faFile} size="5x" /><p className="link-text">My Resume</p></a>
             </div>
           </div>
         </div>
       </section>
 
-      <footer id="footer" class="footer">
-        <h6 class="footer__title white uppercase">Thanks for visiting!</h6>
-        <div class="content">
-          <div class="divider"></div>
-          <div class="u-center">
-            <p>Want to get in touch?<br /><a href="mailto:taylor.abra8@gmail.com" target="_blank" classname="utb utb-OLR">taylor.abra8@gmail.com</a></p>
+      <section id="projects-sec" className="section">
+        <a id="projects" class="anchor" />
+        <div className="content">
+          <h3 className="u-center">Projects</h3>
+          <div className="divider" />
+          <div className="row level">
+            <div className="col col-4">
+              <div className="card">
+                <div className="overlay" />
+                <div className="card__container">
+                  <div className="card__image" />
+                  <div className="card__title-container">
+                    <p className="title">MTG Tracker</p><span className="subtitle">A React Native tracking app for a competitive card game</span></div>
+                </div>
+                <div className="content">
+                  <p>Text and other content belong here.</p>
+                </div>
+              </div>
+            </div>
+            <div className="col col-4">
+              <div className="card">
+                <div className="overlay" />
+                <div className="card__container">
+                  <div className="card__image" />
+                  <div className="card__title-container">
+                    <p className="title">Title</p><span className="subtitle">Subtitle</span></div>
+                </div>
+                <div className="content">
+                  <p>Text and other content belong here.</p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-        <p class="subtitle">© 2021 Taylor Abraham</p>
+      </section>
+
+      <footer id="footer" className="footer">
+        <h6 className="footer__title white uppercase">Thanks for visiting!</h6>
+        <div className="content">
+          <div className="divider" />
+          <div className="u-center">
+            <p>Want to get in touch?<br /><a href="mailto:taylor.abra8@gmail.com" target="_blank" className="utb utb-OLR">taylor.abra8@gmail.com</a></p>
+          </div>
+        </div>
+        <p className="subtitle">© 2021 Taylor Abraham</p>
       </footer>
     </div>
   );
